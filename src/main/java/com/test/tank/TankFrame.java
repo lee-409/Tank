9b50d2d765a29e274755fe3c379664e7b36e1e4b
lihongkun <lihongkun@163.com>
2022-06-04 23:48:56 +0800

    Update TankFrame.java
    变更方向逻辑

--- src/main/java/com/test/tank/TankFrame.java
+++ src/main/java/com/test/tank/TankFrame.java
@@ -36,28 +36,50 @@ public class TankFrame extends Frame {
     }
 
     class MyKeyListener extends KeyAdapter {
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
         @Override
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
             switch (key){
                 case KeyEvent.VK_LEFT:
-                    x -= 10;
+                    bL = true;
                     break;
                 case KeyEvent.VK_UP:
-                    y -= 10;
+                    bU = true;
                     break;
                 case KeyEvent.VK_RIGHT:
-                    x += 10;
+                    bR = true;
                     break;
                 case KeyEvent.VK_DOWN:
-                    y += 10;
+                    bD = true;
+                    break;
+                default:
                     break;
             }
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+            int key = e.getKeyCode();
+            switch (key){
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
