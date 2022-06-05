5f86b1a671b7c42afae71995c4d6786dce37da04
lee-409 <lee_409@163.com>
2022-06-06 00:17:54 +0800

    抽象坦克类，封装坦克类属性和方法，在TankFrame中直接调用

--- src/main/java/com/test/tank/TankFrame.java
+++ src/main/java/com/test/tank/TankFrame.java
@@ -7,9 +7,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
-    int x = 200,y = 200;
-    Dir dir = Dir.DOWN;
-    final static int SPEED = 10;
+    Tank myTank = new Tank(200, 200,Dir.DOWN);
     public TankFrame() {
         setSize(800,600);
         //设置不可改变大小
@@ -29,23 +27,7 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        g.fillRect(x,y,50,50);
-        switch (dir){
-            case LEFT:
-                x -= SPEED;
-                break;
-            case UP:
-                y -= SPEED;
-                break;
-            case RIGHT:
-                x += SPEED;
-                break;
-            case DOWN:
-                y += SPEED;
-                break;
-            default:
-                break;
-        }
+        myTank.paint(g);
     }
 
     class MyKeyListener extends KeyAdapter {
@@ -98,10 +80,10 @@ public class TankFrame extends Frame {
         }
 
         private void setMainTankDir() {
-            if (bL) dir = Dir.LEFT;
-            if (bU) dir = Dir.UP;
-            if (bR) dir = Dir.RIGHT;
-            if (bD) dir = Dir.DOWN;
+            if (bL) myTank.setDir( Dir.LEFT);
+            if (bU) myTank.setDir( Dir.UP);
+            if (bR) myTank.setDir( Dir.RIGHT);
+            if (bD) myTank.setDir( Dir.DOWN);
         }
 
     }
