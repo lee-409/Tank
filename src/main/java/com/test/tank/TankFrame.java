7482e0536ec21c93d365a3675e0fcb3b4591d568
lee-409 <lee_409@163.com>
2022-06-05 23:58:17 +0800

    定义填充方向的enum,根据按键状态控制坦克方向，根据坦克方向控制坦克位移

--- src/main/java/com/test/tank/TankFrame.java
+++ src/main/java/com/test/tank/TankFrame.java
@@ -8,7 +8,8 @@ import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
     int x = 200,y = 200;
-
+    Dir dir = Dir.DOWN;
+    final static int SPEED = 10;
     public TankFrame() {
         setSize(800,600);
         //设置不可改变大小
@@ -28,11 +29,23 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
-        System.out.println("paint");
-        g.fill3DRect(x,y,50,50,true);
-//        g.fillRect(100,100,50,50);
-//        x += 10;
-//        y +=10;
+        g.fillRect(x,y,50,50);
+        switch (dir){
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            default:
+                break;
+        }
     }
 
     class MyKeyListener extends KeyAdapter {
@@ -59,6 +72,7 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
 
         @Override
@@ -80,6 +94,15 @@ public class TankFrame extends Frame {
                 default:
                     break;
             }
+            setMainTankDir();
         }
+
+        private void setMainTankDir() {
+            if (bL) dir = Dir.LEFT;
+            if (bU) dir = Dir.UP;
+            if (bR) dir = Dir.RIGHT;
+            if (bD) dir = Dir.DOWN;
+        }
+
     }
 }
