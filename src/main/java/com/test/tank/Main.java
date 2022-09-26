7951b93acc8e28fff0c7a5138aef51ddeba81d34
lee-409 <lee_409@163.com>
2022-09-26 12:24:44 +0800

    抽象GameModel，将Model与View分离；同事GameModel作为Facade，负责与Frame打交道，负责内部事务

--- src/main/java/com/test/tank/Main.java
+++ src/main/java/com/test/tank/Main.java
@@ -1,7 +1,13 @@
 package com.test.tank;
 
+import java.util.PriorityQueue;
+import java.util.Stack;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+
         TankFrame tf = new TankFrame();
 
         new Thread(()->new Audio("audio/war1.wav").loop()).start();
